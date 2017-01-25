e0abd8cee55b46034f8035d73ba9a5b07ab6d9b8
ifotn <rich@infrontofthenet.com>
2017-01-25 17:39:34 -0500

    end of class files

---
+++ callHello.js
@@ -0,0 +1,9 @@
+/**
+ * Created by RFreeman on 1/25/2017.
+ */
+
+// link to the sayHello file
+let hello = require('./sayHello');
+
+// call the public function in the other file
+hello.sayHello();
---
+++ sayHello.js
@@ -0,0 +1,10 @@
+/**
+ * Created by RFreeman on 1/25/2017.
+ */
+// create a variable with a string value
+let hello = 'Hello World';
+
+// make this function public with the keyword "exports"
+exports.sayHello = function() {
+  console.log(hello);
+};
